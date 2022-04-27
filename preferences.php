976aae8f14a7ca62254a9ae60793e78256c7c596
Alejandra Trujillo <alejandratrujillo@Alejandras-MacBook-Pro.local>
2022-04-26 20:22:12 -0400

    Added signout to preferences page

--- preferences.php
+++ preferences.php
@@ -104,5 +104,20 @@ $conn->close();
 exit();
 ?>
 </div>
+<script>        
+    function signOut() {
+        var auth2 = gapi.auth2.getAuthInstance();
+        auth2.signOut().then(function () {
+            console.log('User signed out.');
+            window.open("/login.html","_self");
+        });
+        }
+
+        function onLoad() {
+        gapi.load('auth2', function() {
+            gapi.auth2.init();
+        });
+    }
+</script>
 </body>
 </html>
